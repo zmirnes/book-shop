dd21699c6e7ef8be798bc1dfba8d9508f1b567a4
zmirnes <zmirnes999@gmail.com>
2023-02-24 23:46:25 +0100

    Initialize footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,8 @@ import WhatReadersSay from "../components/WhatReadersSay/WhatReadersSay";
 import WhatWillYouLearn from "../components/WhatWillYouLearn/WhatWillYouLearn";
 import Blog from "../components/Blog/Blog";
 import Subscribe from "../components/Subscribe/Subscribe";
+import Footer from "../components/Footer/Footer";
+
 const Home = () => {
   return (
     <Fragment>
@@ -24,6 +26,7 @@ const Home = () => {
       <WhatReadersSay />
       <Blog />
       <Subscribe />
+      <Footer />
     </Fragment>
   );
 };
