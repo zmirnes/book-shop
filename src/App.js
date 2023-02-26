93fc3cf33ecd3cb801c8c426c77561623c07ea27
zmirnes <zmirnes999@gmail.com>
2023-02-26 21:26:59 +0100

    Revert old file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NotFoundPage from "./pages/NotFoundPage";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
