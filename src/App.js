bb855ed03d97c4c085afc4fd589678e7cfaedcb0
zmirnes <zmirnes999@gmail.com>
2023-02-26 21:12:22 +0100

    Trying to add React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
