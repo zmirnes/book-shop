71d8f7a65b4ef7fc0b815c286f990f11696d381c
zmirnes <zmirnes999@gmail.com>
2023-02-27 12:25:25 +0100

    Try to fix links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
